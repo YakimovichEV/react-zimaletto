b8884cc86620d58b32aeab480b62f4894592e61b
YakimovichEV <YakimovichJV@gmail.com>
2021-03-28 22:40:33 +0300

    Added a new component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <div className="header__logo">
+                <img src="" alt="" />
+            </div>
+            <div className="header__menu">
+                <ul>
+                    <li>Меню</li>
+                    <li>Доставка</li>
+                    <li>Оплата</li>
+                    <li>Бронь стола</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
