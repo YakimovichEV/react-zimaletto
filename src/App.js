a59531db86d1751951c4684922b2fd2410e8faa0
YakimovichEV <YakimovichJV@gmail.com>
2021-03-28 22:39:48 +0300

    Deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
 import { Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+
 import { Home } from "./pages/Home";
 
 function App() {
     return (
-        <div className="wrapper">
+        <div className="container">
+            <Header />
             <div className="content">
                 <Route path="/" component={Home} exact />
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export const Home = () => {
-    return (
-        <div>
-            <h1>Zimaletto</h1>
-        </div>
-    );
+    return <div></div>;
 };
