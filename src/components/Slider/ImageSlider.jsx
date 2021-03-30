9e444bab5f16a189b112082a1011265de440dfed
YakimovichEV <YakimovichJV@gmail.com>
2021-03-30 23:05:26 +0300

    Create slider

---
+++ src/components/Slider/ImageSlider.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
+import { SliderData } from "./SliderData";
+
+const ImageSlider = ({ slides }) => {
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current - 1);
+    };
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+    return (
+        <section className="slider">
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+            <FaArrowAltCircleRight
+                className="right-arrow"
+                onClick={nextSlide}
+            />
+            {SliderData.map((slide, index) => {
+                return (
+                    <div
+                        className={index === current ? "slide active" : "slide"}
+                        key={index}
+                    >
+                        {index === current && (
+                            <img
+                                src={slide.image}
+                                alt="slider img"
+                                className="slider__img"
+                            />
+                        )}
+                    </div>
+                );
+            })}
+        </section>
+    );
+};
+
+export default ImageSlider;
---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image:
+            "https://images.pexels.com/photos/5601/fashion-shoes-white-snickers.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+    {
+        image:
+            "https://images.pexels.com/photos/5601/fashion-shoes-white-snickers.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+    {
+        image:
+            "https://images.pexels.com/photos/5601/fashion-shoes-white-snickers.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    },
+];
