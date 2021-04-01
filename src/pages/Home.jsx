13fe3c30e238d10a858d6c626a1c9151e9ab0f1f
YakimovichEV <YakimovichJV@gmail.com>
2021-04-01 18:07:23 +0300

    Create component Products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import ImageSlider from "../components/Slider/ImageSlider";
 import { SliderData } from "../components/Slider/SliderData";
 import { Categories } from "../components/Categories/Categories";
+import { Products } from "../components/Products/Products";
 
 export const Home = () => {
     return (
         <div>
             <ImageSlider slides={SliderData} />
             <Categories />
+            <Products />
         </div>
     );
 };
