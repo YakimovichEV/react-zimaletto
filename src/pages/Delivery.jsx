6b199d73777d748ac0d4fe91277b719cafd2d56c
YakimovichEV <YakimovichJV@gmail.com>
2021-04-02 17:25:05 +0300

    Create page "Delivery"

---
+++ src/pages/Delivery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Delivery = () => {
+    return (
+        <div>
+            <h1>Пиривет</h1>
+        </div>
+    );
+};
+
+export default Delivery;
