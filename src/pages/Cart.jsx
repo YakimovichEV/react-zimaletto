745a8d2eade30adb83ae62a515be5e624e54ea9a
YakimovichEV <YakimovichJV@gmail.com>
2021-04-04 16:16:24 +0300

    Upd "Cart" page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Cart = () => {
+    return (
+        <section className="cart">
+            <div className="container">
+                <h1>Корзина</h1>
+
+                <div className="cart__inner">
+                    <div className="cart__item">1</div>
+                    <div className="cart__item">2</div>
+                </div>
+            </div>
+        </section>
+    );
+};
