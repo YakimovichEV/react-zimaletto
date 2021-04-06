bd8553bb96f1f15b7e8fa00a11f351d84e72622d
YakimovichEV <YakimovichJV@gmail.com>
2021-04-06 18:51:35 +0300

    Create modal window

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
+import { Modal } from "../Modal/Modal";
 
 import headerLogo from "../../assets/img/logo.svg";
 import headerPhone from "../../assets/img/phone.svg";
@@ -7,6 +10,25 @@ import headerPerson from "../../assets/img/person.svg";
 import headerCart from "../../assets/img/shopping-cart.svg";
 
 export const Header = () => {
+    const [modal, setModal] = useState({
+        modal1: false,
+        modal2: false,
+    });
+
+    const modalState = () => {
+        setModal({
+            ...modal,
+            modal1: true,
+        });
+    };
+
+    const modalClose = () => {
+        setModal({
+            ...modal,
+            modal1: false,
+        });
+    };
+
     return (
         <div className="header">
             <div className="container">
@@ -47,9 +69,16 @@ export const Header = () => {
                             <img src={headerPhone} alt="" />
                             <span>+7 (831) 282-60-00</span>
                         </div>
-                        <div className="header__user">
+                        <button className="header__user" onClick={modalState}>
+                            <Modal
+                                title={"Modal title"}
+                                isOpened={modal.modal1}
+                                onModalClose={modalClose}
+                            >
+                                asd
+                            </Modal>
                             <img src={headerPerson} alt="" />
-                        </div>
+                        </button>
                         <div className="header__cart">
                             <img src={headerCart} alt="" />
                         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import ImageSlider from "../components/Slider/ImageSlider";
 import { SliderData } from "../components/Slider/SliderData";
