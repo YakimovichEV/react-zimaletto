c51a171fbf2620aa079ecd2a24ab4fed046ed35f
YakimovichEV <YakimovichJV@gmail.com>
2021-04-20 20:47:49 +0300

    Create Form for "Modal" component

---
+++ src/components/Modal/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { FormSignup } from "./FormSignup";
+
+export const Form = () => {
+    return (
+        <div>
+            <FormSignup />
+        </div>
+    );
+};
---
+++ src/components/Modal/FormSignup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FormSignUp = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+        </div>
+    );
+};
